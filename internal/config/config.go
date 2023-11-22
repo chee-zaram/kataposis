17b87c7e062925d690a69076e80a2cd387f2064b
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2023-11-22 18:10:15 +0100

    internal/config/: remove regular config pattern

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,5 @@
 package config
 
-import (
-	"errors"
-	"os"
-)
-
 type DBConfig struct {
 	PGUser   string
 	PGPass   string
@@ -12,40 +7,7 @@ type DBConfig struct {
 	PGDBName string
 }
 
-const (
-	pgUser   = "KTPSS_PG_USER"
-	pgPass   = "KTPSS_PG_PASS"
-	pgAddr   = "KTPSS_PG_ADDR"
-	pgDBName = "KTPSS_PG_DB"
-)
-
-// NewConfig returns a configuration struct based on the current values of the
-// environment variables.
-func NewConfig() (*DBConfig, error) {
-	user, ok := os.LookupEnv(pgUser)
-	if !ok {
-		return nil, errors.New("database user not set")
-	}
-
-	pass, ok := os.LookupEnv(pgPass)
-	if !ok {
-		return nil, errors.New("database password not set")
-	}
-
-	addr, ok := os.LookupEnv(pgAddr)
-	if !ok {
-		return nil, errors.New("database address not set")
-	}
-
-	db, ok := os.LookupEnv(pgDBName)
-	if !ok {
-		return nil, errors.New("database name not set")
-	}
-
-	return &DBConfig{
-		PGUser:   user,
-		PGPass:   pass,
-		PGAddr:   addr,
-		PGDBName: db,
-	}, nil
+// NewEmptyDBConfig returns a new empty database configuration struct.
+func NewEmptyDBConfig() *DBConfig {
+	return &DBConfig{}
 }
